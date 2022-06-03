93a073536e47518f384c4e30b98045d1664f8e38
Kirill Vercetti <office@kyzima-spb.com>
2022-06-03 21:07:01 +0300

    Remove MimeType validator

--- src/flask_uploader/validators.py
+++ src/flask_uploader/validators.py
@@ -15,7 +15,6 @@ __all__ = (
     'FileRequired',
     'FileSize',
     'ImageSize',
-    'MimeType',
     'TValidator',
 )
 
@@ -278,70 +277,3 @@ class ImageSize:
             raise ValidationError(
                 self.format_message(message, width=width, height=height)
             )
-
-
-class MimeType:
-    """
-    The validator checks the media type passed in the Content-Type HTTP header.
-
-    This is a weak type of validation.
-    """
-
-    # This just contains plain text files
-    TEXT = frozenset((
-        formats.TXT.mimetype,
-    ))
-
-    # This contains various office document formats
-    MS_OFFICE = frozenset(f.mimetype for f in formats.MS_OFFICE)
-    OPEN_OFFICE = frozenset(f.mimetype for f in formats.OPEN_OFFICE)
-    WPS_OFFICE = frozenset(f.mimetype for f in formats.WPS_OFFICE)
-    OFFICE = MS_OFFICE | OPEN_OFFICE | WPS_OFFICE | {
-        f.mimetype for f in formats.OTHER_OFFICE
-    }
-
-    # This contains electronic book formats
-    EDOCUMENTS = frozenset(f.mimetype for f in formats.EDOCUMENTS)
-    EBOOKS = frozenset(f.mimetype for f in formats.EBOOKS)
-    BOOKS = EDOCUMENTS | EBOOKS
-
-    # This contains basic image types that are viewable from most browsers
-    IMAGES = frozenset(f.mimetype for f in formats.IMAGES)
-
-    # This contains audio file types
-    AUDIO = frozenset(f.mimetype for f in formats.AUDIO)
-
-    # This is for structured data files
-    DATA = frozenset(f.mimetype for f in formats.DATA)
-
-    # This contains various types of scripts
-    SCRIPTS = frozenset(f.mimetype for f in formats.SCRIPTS)
-
-    # This contains archive and compression formats
-    ARCHIVES = frozenset(f.mimetype for f in formats.ARCHIVES)
-
-    # This contains non executable source files
-    # those which need to be compiled or assembled to binaries to be used.
-    # They are generally safe to accept, as without an existing RCE vulnerability,
-    # they cannot be compiled, assembled, linked, or executed.
-    SOURCE = frozenset(f.mimetype for f in formats.SOURCE)
-
-    # Shared libraries and executable file formats
-    EXECUTABLES = frozenset(f.mimetype for f in formats.EXECUTABLES)
-
-    def __init__(
-        self,
-        mime_types: t.Union[set[str], frozenset[str]],
-        message: t.Optional[str] = None,
-    ) -> None:
-        self.mime_types = mime_types
-        self.message = message
-
-    def __call__(self, storage: FileStorage) -> None:
-        if storage.mimetype is None:
-            message = self.message or 'There is no HTTP Content-Type header.'
-            raise ValidationError(message)
-
-        if storage.mimetype not in self.mime_types:
-            message = self.message or 'This file type is not allowed to be uploaded.'
-            raise ValidationError(message)
