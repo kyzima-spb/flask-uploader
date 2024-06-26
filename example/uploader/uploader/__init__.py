2dc2f785ab4559be0e73148d5155ff805e9734d3
Kirill Vercetti <office@kyzima-spb.com>
2024-06-27 02:21:58 +0300

    Example dependency upgrade

--- example/uploader/uploader/__init__.py
+++ example/uploader/uploader/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
 from flask_uploader import init_uploader
+from flask_useful.app import (
+    config_from_prefixed_env,
+    config_from_secrets_env,
+)
 
 from . import routes
 from .extensions import csrf, login_manager, aws
@@ -8,7 +12,8 @@ from .models import mongo
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_pyfile('config.py')
+    config_from_prefixed_env(app)
+    config_from_secrets_env(app)
 
     csrf.init_app(app)
     login_manager.init_app(app)
